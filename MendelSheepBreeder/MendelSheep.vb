52d0183f938e5644b0a16f39fbbeec2a62ad6c45
Michael Senn <m.senn93@gmail.com>
2012-01-15 20:14:10 +0100

    Added MendelSheep and PhenotypeMap classes, mapped genotypes to Phenotypes in PhenotypeMap

---
+++ MendelSheepBreeder/MendelSheep.vb
@@ -0,0 +1,44 @@
+﻿Friend Class MendelSheep
+
+#Region "Properties"
+
+    Friend Property Genom As Genome
+
+        Get
+            Return _Genom
+        End Get
+
+        Set(value As Genome)
+            _Genom = value
+            UpdatePhenotype()
+        End Set
+
+    End Property
+
+    Private _Genom As Genome
+
+
+    Friend ReadOnly Property Phenotype As Phenotype
+
+        Get
+            Return _Phenotype
+        End Get
+
+    End Property
+
+    Private _Phenotype As Phenotype
+
+#End Region
+
+#Region "New"
+
+#End Region
+
+
+    Private Sub UpdatePhenotype()
+
+    End Sub
+
+
+
+End Class
---
+++ MendelSheepBreeder/PhenotypeMap.vb
@@ -0,0 +1,28 @@
+﻿Friend Class PhenotypeMap
+
+    Friend Map(,,,) As Phenotype
+
+    Friend Sub New()
+
+        ' Maps Genome-values (actually only the resulting ones, that is after taking into to account recessive / dominant mechanisms) to Phenotypes.
+        ' Order of genomes is: Naturalness -Colourness - Lightness - Hue.
+        Map(0, 0, 0, 0) = Phenotype.Blue)
+        Map(0, 0, 0, 1) = Phenotype.Purple
+        Map(0, 0, 1, 0) = Phenotype.LightBlue
+        Map(0, 0, 1, 1) = Phenotype.Cyan
+        Map(0, 1, 0, 0) = Phenotype.Brown
+        Map(0, 1, 0, 1) = Phenotype.Purple
+        Map(0, 1, 1, 0) = Phenotype.Red
+        Map(0, 1, 1, 1) = Phenotype.Pink
+        Map(1, 0, 0, 0) = Phenotype.Green
+        Map(1, 0, 0, 1) = Phenotype.Orange
+        Map(1, 0, 1, 0) = Phenotype.Green
+        Map(1, 0, 1, 1) = Phenotype.Yellow
+        Map(1, 1, 0, 0) = Phenotype.Black
+        Map(1, 1, 0, 1) = Phenotype.LightGrey
+        Map(1, 1, 1, 0) = Phenotype.Grey
+        Map(1, 1, 1, 1) = Phenotype.White
+
+    End Sub
+
+End Class
