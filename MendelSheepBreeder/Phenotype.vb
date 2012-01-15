3eb341104fcff3d9ec3cafe1ef033055cfbfceaf
Michael Senn <m.senn93@gmail.com>
2012-01-15 18:14:46 +0100

    Added phenoytpe enum

---
+++ MendelSheepBreeder/Phenotype.vb
@@ -0,0 +1,20 @@
+﻿Friend Enum Phenotype
+
+    White
+    LightGray
+    Grey
+    Black
+    Brown
+    Pink
+    Red
+    Orange
+    Yellow
+    Lime
+    Green
+    LightBlue
+    Cyan
+    Blue
+    Purple
+    Magenta
+
+End Enum
