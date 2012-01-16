e953d3da9b29c15e002c2179f5bd9a98032fbbb4
Michael Senn <m.senn93@gmail.com>
2012-01-16 19:16:49 +0100

    Added Allele class

---
+++ MendelSheepBreeder/Allele.vb
@@ -0,0 +1,28 @@
+﻿Friend Class Allele
+
+#Region "Properties"
+
+    Friend Property Gene1 As Byte
+
+        Get
+            Return _Gene1
+        End Get
+
+        Set(value As Byte)
+            Select Case _Gene1
+                Case 0
+                    _Gene1 = 0
+                Case 1
+                    _Gene1 = 1
+                Case Else
+                    Throw New ArgumentOutOfRangeException("Gene1", "Has to be either 0 or 1")
+            End Select
+        End Set
+
+    End Property
+
+    Private _Gene1 As Byte
+
+#End Region
+
+End Class
