bb261925c70048d0db9c2bd362b0c1945ea288f8
Michael Senn <m.senn93@gmail.com>
2012-01-17 21:30:42 +0100

    Cleared root namespace setting

--- MendelSheepBreeder/My Project/Application.Designer.vb
+++ MendelSheepBreeder/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ch.Morrolan.Minecraft.MendelSheepBreeder.frmMain
+            Me.MainForm = Global.frmMain
         End Sub
     End Class
 End Namespace
--- MendelSheepBreeder/My Project/Settings.Designer.vb
+++ MendelSheepBreeder/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.ch.Morrolan.Minecraft.MendelSheepBreeder.My.MySettings
+        Friend ReadOnly Property Settings() As Global.My.MySettings
             Get
-                Return Global.ch.Morrolan.Minecraft.MendelSheepBreeder.My.MySettings.Default
+                Return Global.My.MySettings.Default
             End Get
         End Property
     End Module
