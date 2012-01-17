ff0f2871d83d1272179ed4c444757bd4ee1e27a2
Michael Senn <m.senn93@gmail.com>
2012-01-17 20:27:17 +0100

    Created basic layout of the main GUI

--- MendelSheepBreeder/frmMain.Designer.vb
+++ MendelSheepBreeder/frmMain.Designer.vb
@@ -22,9 +22,264 @@ Partial Class frmMain
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.tblMain = New System.Windows.Forms.TableLayoutPanel()
+        Me.tblParentsInput = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pnlParentsGenomeP2 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtParentsGenomeP1Naturalness = New System.Windows.Forms.TextBox()
+        Me.pnlParentsGenomeP1 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.tblMain.SuspendLayout()
+        Me.tblParentsInput.SuspendLayout()
+        Me.pnlParentsGenomeP1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tblMain
+        '
+        Me.tblMain.ColumnCount = 2
+        Me.tblMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblMain.Controls.Add(Me.tblParentsInput, 0, 0)
+        Me.tblMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tblMain.Location = New System.Drawing.Point(0, 0)
+        Me.tblMain.Name = "tblMain"
+        Me.tblMain.RowCount = 2
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblMain.Size = New System.Drawing.Size(1023, 630)
+        Me.tblMain.TabIndex = 0
+        '
+        'tblParentsInput
+        '
+        Me.tblParentsInput.ColumnCount = 2
+        Me.tblParentsInput.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.86139!))
+        Me.tblParentsInput.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.13861!))
+        Me.tblParentsInput.Controls.Add(Me.Label1, 0, 0)
+        Me.tblParentsInput.Controls.Add(Me.pnlParentsGenomeP2, 1, 1)
+        Me.tblParentsInput.Controls.Add(Me.pnlParentsGenomeP1, 1, 0)
+        Me.tblParentsInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tblParentsInput.Location = New System.Drawing.Point(3, 3)
+        Me.tblParentsInput.Name = "tblParentsInput"
+        Me.tblParentsInput.RowCount = 2
+        Me.tblParentsInput.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblParentsInput.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tblParentsInput.Size = New System.Drawing.Size(505, 309)
+        Me.tblParentsInput.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'pnlParentsGenomeP2
+        '
+        Me.pnlParentsGenomeP2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlParentsGenomeP2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlParentsGenomeP2.Location = New System.Drawing.Point(173, 157)
+        Me.pnlParentsGenomeP2.Name = "pnlParentsGenomeP2"
+        Me.pnlParentsGenomeP2.Size = New System.Drawing.Size(329, 149)
+        Me.pnlParentsGenomeP2.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Label5.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Label5, 2)
+        Me.Label5.Location = New System.Drawing.Point(270, 8)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Hue"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Label4.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Label4, 2)
+        Me.Label4.Location = New System.Drawing.Point(174, 8)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Lightness"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Label3.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Label3, 2)
+        Me.Label3.Location = New System.Drawing.Point(90, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Colourness"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Label2.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Label2, 2)
+        Me.Label2.Location = New System.Drawing.Point(8, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Naturalness"
+        '
+        'txtParentsGenomeP1Naturalness
+        '
+        Me.txtParentsGenomeP1Naturalness.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtParentsGenomeP1Naturalness.Location = New System.Drawing.Point(3, 24)
+        Me.txtParentsGenomeP1Naturalness.Name = "txtParentsGenomeP1Naturalness"
+        Me.txtParentsGenomeP1Naturalness.Size = New System.Drawing.Size(34, 20)
+        Me.txtParentsGenomeP1Naturalness.TabIndex = 0
+        '
+        'pnlParentsGenomeP1
+        '
+        Me.pnlParentsGenomeP1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlParentsGenomeP1.Controls.Add(Me.TableLayoutPanel1)
+        Me.pnlParentsGenomeP1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlParentsGenomeP1.Location = New System.Drawing.Point(173, 3)
+        Me.pnlParentsGenomeP1.Name = "pnlParentsGenomeP1"
+        Me.pnlParentsGenomeP1.Size = New System.Drawing.Size(329, 148)
+        Me.pnlParentsGenomeP1.TabIndex = 3
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 8
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox7, 7, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox6, 6, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox5, 5, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox4, 4, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox3, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox2, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox1, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.txtParentsGenomeP1Naturalness, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label5, 6, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label4, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 2, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.38356!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.61644!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(327, 146)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(43, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Location = New System.Drawing.Point(83, 24)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox3.Location = New System.Drawing.Point(123, 24)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox4.Location = New System.Drawing.Point(163, 24)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox5.Location = New System.Drawing.Point(203, 24)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox6.Location = New System.Drawing.Point(243, 24)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(34, 20)
+        Me.TextBox6.TabIndex = 10
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox7.Location = New System.Drawing.Point(283, 24)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox7.TabIndex = 11
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1023, 630)
+        Me.Controls.Add(Me.tblMain)
+        Me.Name = "frmMain"
+        Me.Text = "Mendel Sheep Breeder"
+        Me.tblMain.ResumeLayout(False)
+        Me.tblParentsInput.ResumeLayout(False)
+        Me.tblParentsInput.PerformLayout()
+        Me.pnlParentsGenomeP1.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents tblMain As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents tblParentsInput As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents pnlParentsGenomeP2 As System.Windows.Forms.Panel
+    Friend WithEvents txtParentsGenomeP1Naturalness As System.Windows.Forms.TextBox
+    Friend WithEvents pnlParentsGenomeP1 As System.Windows.Forms.Panel
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- MendelSheepBreeder/frmMain.vb
+++ MendelSheepBreeder/frmMain.vb
@@ -1,4 +1,4 @@
 ﻿Public Class frmMain
-   
+
 End Class
 
