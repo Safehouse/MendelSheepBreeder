e0b048800e734fa59f68b75f7c4167ca566d2822
Michael Senn <m.senn93@gmail.com>
2012-01-21 20:51:12 +0100

    Added new class to take care of the statistics

---
+++ MendelSheepBreeder/SheepStatistic.vb
@@ -0,0 +1,160 @@
+﻿Namespace ch.Morrolan.Minecraft.MendelSheepBreeder
+
+    Friend Class SheepStatistic
+
+#Region "Properties"
+
+        Friend ReadOnly Property CountWhite As Byte
+
+            Get
+                Return _CountWhite
+            End Get
+
+        End Property
+
+        Private _CountWhite As Byte
+
+
+        Friend ReadOnly Property CountLightGrey As Byte
+
+            Get
+                Return _CountLightGrey
+            End Get
+
+        End Property
+        Private _CountLightGrey As Byte
+
+        Friend ReadOnly Property CountGrey As Byte
+
+            Get
+                Return _CountGrey
+            End Get
+
+        End Property
+        Private _CountGrey As Byte
+
+        Friend ReadOnly Property CountBlack As Byte
+
+            Get
+                Return _CountBlack
+            End Get
+
+        End Property
+        Private _CountBlack As Byte
+
+        Friend ReadOnly Property CountBrown As Byte
+
+            Get
+                Return _CountBrown
+            End Get
+
+        End Property
+        Private _CountBrown As Byte
+
+        Friend ReadOnly Property CountPink As Byte
+
+            Get
+                Return _CountPink
+            End Get
+
+        End Property
+        Private _CountPink As Byte
+
+        Friend ReadOnly Property CountRed As Byte
+
+            Get
+                Return _CountRed
+            End Get
+
+        End Property
+        Private _CountRed As Byte
+
+        Friend ReadOnly Property CountOrange As Byte
+
+            Get
+                Return _CountOrange
+            End Get
+
+        End Property
+        Private _CountOrange As Byte
+
+        Friend ReadOnly Property CountYellow As Byte
+
+            Get
+                Return _CountYellow
+            End Get
+
+        End Property
+        Private _CountYellow As Byte
+
+        Friend ReadOnly Property CountLime As Byte
+
+            Get
+                Return _CountLime
+            End Get
+
+        End Property
+        Private _CountLime As Byte
+
+        Friend ReadOnly Property CountGreen As Byte
+
+            Get
+                Return _CountGreen
+            End Get
+
+        End Property
+        Private _CountGreen As Byte
+
+        Friend ReadOnly Property CountLightBlue As Byte
+
+            Get
+                Return _CountLightBlue
+            End Get
+
+        End Property
+        Private _CountLightBlue As Byte
+
+        Friend ReadOnly Property CountCyan As Byte
+
+            Get
+                Return _CountCyan
+            End Get
+
+        End Property
+        Private _CountCyan As Byte
+
+        Friend ReadOnly Property CountBlue As Byte
+
+            Get
+                Return _CountBlue
+            End Get
+
+        End Property
+        Private _CountBlue As Byte
+
+        Friend ReadOnly Property CountPurple As Byte
+
+            Get
+                Return _CountPurple
+            End Get
+
+        End Property
+        Private _CountPurple As Byte
+
+        Friend ReadOnly Property CountMagenta As Byte
+
+            Get
+                Return _CountMagenta
+            End Get
+
+        End Property
+        Private _CountMagenta As Byte
+
+
+
+#End Region
+
+
+    End Class
+
+End Namespace
